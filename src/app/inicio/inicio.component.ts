ef41093c5d99123b5e7d301edd6427208c9efedd
Madness2002 <73316834+Madness2002@users.noreply.github.com>
2023-08-07 00:40:50 -0500

    Implementación del diseño responsive de la pagina de inicio (90%)

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  vOpciones?:boolean;
+  constructor(public router: Router) {
+  }
 
+  IrA(url:string): void{
+
+    this.router.navigate([url])
+  }
+  Validador(): void{
+
+    this.vOpciones= !this.vOpciones;
+  }
+  GetValidador():boolean{
+    // @ts-ignore
+    return this.vOpciones;
+  }
 }
